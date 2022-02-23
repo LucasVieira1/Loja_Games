158f0d943af569d11bd9cb1bd2b3bbf4c3ffc55f
LucasVieira1 <vieiraalucas1@gmail.com>
2022-02-23 16:42:23 -0300

    Exercicio

---
+++ src/main/java/com/example/lojagames/controller/ProdutoController.java
@@ -0,0 +1,79 @@
+package com.example.lojagames.controller;
+
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.lojagames.model.Produtos;
+import com.example.lojagames.repository.CategoriaRepository;
+import com.example.lojagames.repository.ProdutoRepository;
+
+@RestController
+@RequestMapping("/produtos")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+public class ProdutoController {
+
+	@Autowired
+	private ProdutoRepository produtoRepository;
+
+	@Autowired // cria a injeção de dependencia
+	private CategoriaRepository categoriaRepository;
+
+	@GetMapping
+	public ResponseEntity<List<Produtos>> getAll() {
+		return ResponseEntity.ok(produtoRepository.findAll());
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<Produtos> getById(@PathVariable Long id) {
+		return produtoRepository.findById(id).map(resposta -> ResponseEntity.ok(resposta))
+				.orElse(ResponseEntity.notFound().build());
+	}
+
+	@GetMapping("/nome/{nome}")
+	public ResponseEntity<List<Produtos>> getByNome(@PathVariable String nome) {
+		return ResponseEntity.ok(produtoRepository.findAllByNomeContainingIgnoreCase(nome));
+	}
+
+	@PostMapping
+	public ResponseEntity<Produtos> postProduto(@Valid @RequestBody Produtos produtos) {
+		if (produtoRepository.existsById(produtos.getCategoria().getId())) {
+			return ResponseEntity.status(HttpStatus.CREATED).body(produtoRepository.save(produtos));
+		} else {
+			return ResponseEntity.notFound().build();
+		}
+	}
+
+	@PutMapping
+	public ResponseEntity<Produtos> putPostagem(@Valid @RequestBody Produtos produtos) {
+		if (categoriaRepository.existsById(produtos.getCategoria().getId())) {
+			return produtoRepository.findById(produtos.getId())
+					.map(resposta -> ResponseEntity.ok().body(produtoRepository.save(produtos)))
+					.orElse(ResponseEntity.notFound().build());
+		} else {
+			return ResponseEntity.notFound().build();
+		}
+	}
+
+	@DeleteMapping("/{id}")
+	public ResponseEntity<?> deleteProduto(@PathVariable Long id) {
+		return produtoRepository.findById(id).map(resposta -> {
+			produtoRepository.deleteById(id);
+			return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+		}).orElse(ResponseEntity.notFound().build());
+	}
+
+}
---
+++ src/main/java/com/example/lojagames/model/Produtos.java
@@ -0,0 +1,74 @@
+package com.example.lojagames.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name = "tb_produtos")
+public class Produtos {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank(message = "O nome não pode retornar vazio!")
+    private String nome;
+	
+	@NotBlank(message = "A descricao não pode retornar vazia!")
+    private String descricao;
+	
+   @Min(0)
+	private double preco;
+	
+	@NotBlank(message = "O console não pode retornar vazio!")
+    private String console;
+    
+	@ManyToOne
+	@JsonIgnoreProperties("produto")
+	private Categoria categoria;
+	
+    public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public double getPreco() {
+		return preco;
+	}
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+	public String getConsole() {
+		return console;
+	}
+	public void setConsole(String console) {
+		this.console = console;
+	}
+	public Categoria getCategoria() {
+		return categoria;
+	}
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+}
